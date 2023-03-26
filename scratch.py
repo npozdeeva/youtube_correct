771e61aa5d164621c5ae8d988c7670ea25fd9217
natalie <natalie@example.com>
2023-03-27 01:36:41 +0300

    update

--- scratch.py
+++ scratch.py
@@ -14,7 +14,7 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.description = self.channel_info['items'][0]['snippet']['description']
-        self.url = 'https://www.youtube.com/' + self.__channel_id
+        self.url = 'https://www.youtube.com/channel/' + self.__channel_id
         self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
@@ -38,22 +38,19 @@ class Channel:
             json.dump(data, file, indent=2, ensure_ascii=False)
 
 #channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'    # Редакция
-pivovarov = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
-#vdud = Channel('')
-pivovarov.print_info()
+# pivovarov = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+# #vdud = Channel('')
+# pivovarov.print_info()
 #vdud.print_info()
 print("Task2:")
 vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 
-# получаем значения атрибутов
-print(vdud.title)
-print(vdud.video_count)
 print(vdud.url)
 # менять не можем
-vdud.channel_id= 'Новое название'
-
-# можем получить объект для работы с API вне класса
-print(Channel.get_service())
-
-# создать файл 'vdud.json' в данными по каналу
-vdud.to_json()
+# vdud.channel_id= 'Новое название'
+#
+# # можем получить объект для работы с API вне класса
+# print(Channel.get_service())
+#
+# # создать файл 'vdud.json' в данными по каналу
+# vdud.to_json()
